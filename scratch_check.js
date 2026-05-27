c7a4eac9977a5fa115b51905f4509499d9f9abab
Amarasimhareddy Avula <amarasimha2001@gmail.com>
2026-05-27 07:11:44 +0530

    fix/update: modified Examresult.JS, Examroutes.JS, scratch_check.js and other files

--- scratch_check.js
+++
@@ -1,8 +0,0 @@
-const { readDB } = require('./utils/localCache');
-const db = readDB();
-console.log('Results in local database:', db.results.map(r => ({
-  _id: r._id,
-  employee: r.employee,
-  assessment: r.assessment,
-  status: r.status
-})));
