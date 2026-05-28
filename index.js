4b242ec2fd43eb0fee9301b6c94de2b139243785
Amarasimhareddy Avula <amarasimha2001@gmail.com>
2026-05-28 06:59:58 +0530

    Hardcode frontend API and socket URL to prevent environment variable misconfiguration

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(compression());
 
 app.use(cors({
   origin: [
-      process.env.CLIENT_URL
+      process.env.CLIENT_URL || "https://onlinetest-vpb4.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
@@ -354,7 +354,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
       origin: [
-          process.env.CLIENT_URL
+          process.env.CLIENT_URL || "https://onlinetest-vpb4.onrender.com"
       ],
       methods: ["GET", "POST"],
       credentials: true
